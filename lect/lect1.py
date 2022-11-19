b739500b3169ee96820df80a840224e0cef90481
AlexeyBolt <boltava.av@gmail.com>
2022-11-19 16:05:56 +0300

    Игра в Очко

--- lect/lect1.py
+++ lect/lect1.py
@@ -13,5 +13,28 @@
 # print(b+a)
 # print(b*a)
 # print(b**a)
+koloda = [6,7,8,9,10,2,3,4,11] * 4
+import random
+random.shuffle(koloda)
+print('Поиграем в очко?')
+count = 0
 
-help (int)
+while True:
+    choice = input('Будете брать карту? y/n\n')
+    if choice == 'y':
+        current = koloda.pop()
+        print('Вам попалась карта достоинством %d' %current)
+        count += current
+        if count > 21:
+            print('Извините, но вы проиграли')
+            break
+        elif count == 21:
+            print('Поздравляю, вы набрали 21!')
+            break
+        else:
+            print('У вас %d очков.' %count)
+    elif choice == 'n':
+        print('У вас %d очков и вы закончили игру.' %count)
+        break
+
+print('До новых встреч!')
