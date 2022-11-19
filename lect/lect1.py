ad169bc0e7bd4efb925c41cd0c091a5100ee6f7c
AlexeyBolt <boltava.av@gmail.com>
2022-11-19 15:58:39 +0300

    вычисления

--- lect/lect1.py
+++ lect/lect1.py
@@ -1,12 +1,17 @@
 #a = "hello "
 #b = "World"
 #print(a+b)
-username = input('Введи имя, бедолага: ')
-if (username == 'Маша'):
-    print ('Маша, раздевайся!')
-else:
-    print ("Нахер Машу, привет ", username);
+# username = input('Введи имя, бедолага: ')
+#  if (username == 'Маша'):
+#      print ('Маша, раздевайся!')
+#  else:
+#      print ("Нахер Машу, привет ", username);
 
 
+# a=int(input("Введи число А: "))
+# b=int(input("Введи число B: "))
+# print(b+a)
+# print(b*a)
+# print(b**a)
 
-
+help (int)
