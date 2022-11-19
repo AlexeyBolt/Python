6a6f164f8c26a950b6f4f853e7cf03c0474f290a
AlexeyBolt <boltava.av@gmail.com>
2022-11-19 15:25:59 +0300

    Хеллоу Маша

--- lect/lect1.py
+++ lect/lect1.py
@@ -1,3 +1,12 @@
-a = "hello "
-b = "World"
-print(a+b)
+#a = "hello "
+#b = "World"
+#print(a+b)
+username = input('Введи имя, бедолага: ')
+if (username == 'Маша'):
+    print ('Маша, раздевайся!')
+else:
+    print ("Нахер Машу, привет ", username);
+
+
+
+
