58703d13ad22c8ce79e6ed45d97b4d9379100238
AlexeyBolt <boltava.av@gmail.com>
2022-11-24 21:19:56 +0300

    решение 2й задачи

--- homework_1/ex_2.py
+++ homework_1/ex_2.py
@@ -2,3 +2,12 @@
 # ⋁ - "Или"
 # ⋀ - "И"
 # ¬ - "Не"
+# for x in [True,False]:
+#     for y in [True,False]:
+#         for z in [True,False]:
+#             print(x,'and',y,'or',z,'=',x and y or z)
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not (x or y or z)==(not x and not y and not z))
+            print(x,y,z,)
