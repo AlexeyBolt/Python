7a9b2ca1acec5aed7aa259495d34c52d668cfd01
AlexeyBolt <boltava.av@gmail.com>
2022-11-27 20:42:17 +0300

    Добавил решение 1й задачи

--- homework_2/ex_2_1.py
+++ homework_2/ex_2_1.py
@@ -4,3 +4,19 @@
 
 # - 6782 -> 23
 # - 0,56 -> 11
+
+import os 
+os.system('cls')
+# import math
+
+
+a_list = list(input("Введите число: "))
+
+if a_list.count(',')>0:
+    a_list.remove(',')
+
+# print(a_list) проверка
+a_list = map(int,a_list)
+sumOfElements=sum(a_list)
+
+print("сумма элементов числа = ", sumOfElements)
