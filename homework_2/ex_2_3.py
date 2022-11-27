9083bddbef9334117e917c9935762adeb8c3acf3
AlexeyBolt <boltava.av@gmail.com>
2022-11-27 21:56:48 +0300

    Добавил решение 3й задачи

--- homework_2/ex_2_3.py
+++ homework_2/ex_2_3.py
@@ -3,3 +3,17 @@
 # Пример:
 
 # - Для n = 6: [2.0, 2.25, 2.37037037037037, 2.44140625, 2.4883199999999994, 2.5216263717421135]
+
+import os 
+os.system('cls')
+import math
+
+n=int(input('введите число, для выявления списка элементов последовательностей: '))
+list_posled = []
+count = 0
+for i in range(1, n+1):
+    count=(1+1/i)**i
+    list_posled.append((count))
+
+print(f'последовательность (1+1/n)^n для n={n} выглядит так:')
+print(list_posled)
