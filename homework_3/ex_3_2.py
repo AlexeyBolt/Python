066134076fb0843a8380950678af06bcd48ee225
AlexeyBolt <boltava.av@gmail.com>
2022-12-04 21:12:45 +0300

    Добавил решение 2й задачи 3й домашки

--- homework_3/ex_3_2.py
+++ homework_3/ex_3_2.py
@@ -6,3 +6,16 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
+import os 
+os.system('cls')
+
+# my_list = [int(elements) for elements in input('Введите элементы массива через пробел: ').split()]
+
+def mult_lst(lst):
+    l = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+    print(new_lst)
+
+
+my_list = [int(elements) for elements in input('Введите элементы массива через пробел: ').split()]
+mult_lst(my_list)
